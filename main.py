fb994aa097d7596f7357d61ef078a6236c25aa87
Your Name <you@example.com>
2021-09-15 14:47:38 +0500

    Добавил конфигурацию через консоль

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
+import asyncio
+import argparse
+
+import aioreloader
 from aiohttp import web
 from mainapp import create_app
 
+try:
+    import uvloop
+    asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+except ImportError:
+    print("Library uvloop is not available.")
+
+parser = argparse.ArgumentParser(description="Aiohttp project")
+parser.add_argument("--host", help="YOUR HOST", default="localhost")
+parser.add_argument("--port", help="YOUR PORT", default=8000)
+parser.add_argument("--reload", action="store_true",
+                    help="Автоперезагразка сервера при изменении в коде")
+
+
+args = parser.parse_args()
+
 app = create_app()
+if args.reload:
+    print("Start with code reload")
+    aioreloader.start()
+
 if __name__ == "__main__":
-    web.run_app(app, host="localhost", port=8000)
+    web.run_app(app, host=args.host, port=args.port)
