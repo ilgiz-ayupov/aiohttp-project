12939de5dc457997a0f834425b04da5111fd07de
ilgiz <ilgizayupov7@gmail.com>
2021-09-18 01:05:45 +0500

    настройка webhook

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,12 +1,18 @@
 from aiohttp import web
 from aiohttp_jinja2 import template
+from aiogram import types
+
+from . import bot
 
 
 @template("index.html")
-async def index(request):
+async def index(request: web.Request):
     print(request)
     return {"username": "Ильгиз"}
 
 
-async def webhook(request):
-    print(request)
+async def webhook(request: web.Request):
+    json_string = request.json()
+    update = types.Update().as_json()
+    bot.dp.process_update(update)
+    return "!", 200
