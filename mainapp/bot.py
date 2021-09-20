b21a125f5f9a61017cce994f2d79e18c0a2dbb35
ilgiz <ilgizayupov7@gmail.com>
2021-09-21 00:12:37 +0500

    Добавил правила бота

--- mainapp/bot.py
+++ mainapp/bot.py
@@ -19,7 +19,22 @@ dp.middleware.setup(LoggingMiddleware())
 async def start(message: types.Message):
     chat_id = message.chat.id
     first_name = message.chat.first_name
-    await bot.send_message(chat_id, text=f"Привет {first_name}")
+    text = f"""Привет {first_name}!
+Вас приветствует Бот-Викторина
+Правила бота простые:
+1. Бот авторизует вас. Для этого бот автоматически соберёт следующее данные:
+    Имя
+    Фамилию
+    Имя пользователя
+    Телеграм ID
+    
+2. Викторина начнётся, когда игрок нажмёт на кнопку "Начать викторину"
+3. На каждый вопрос будет дано по 4 варианта ответа, в качестве кнопок
+4. Вопрос не ограничен по времени
+4. Нажимайте на кнопки, что бы ответить на вопрос. Иначе вам засчитается неверный ответ !
+5. Выигрывает тот игрок который ответит правильно больше остальных игроков.
+"""
+    await bot.send_message(chat_id, text=text)
     await register_user(message)
     await bot.send_message(chat_id, "Начать викторину ?", reply_markup=keyboards.generate_quiz_start_menu())
 
